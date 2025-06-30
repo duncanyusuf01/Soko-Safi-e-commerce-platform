f418443fae40d53f772345293baa67485e79884b
Duncan Kariuki <duncankariukimugo@gmail.com>
2025-06-30 14:57:23 +0300

    changes postgress URL to internal

--- server/config.py
+++ server/config.py
@@ -18,6 +18,10 @@ db = SQLAlchemy(metadata=metadata)
 # Get the absolute path of the directory the script is in
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 # Define the database URI
+# DATABASE_URI = os.environ.get(
+#     "DB_URI", f"postgresql://soko_safi_db_user:HMVhb7WW5W7N02Ki7VFweUKv7IFaOQfM@dpg-d1go76fgi27c73bvasug-a.oregon-postgres.render.com/soko_safi_db"
+# )
+
 DATABASE_URI = os.environ.get(
     "DB_URI", f"postgresql://soko_safi_db_user:HMVhb7WW5W7N02Ki7VFweUKv7IFaOQfM@dpg-d1go76fgi27c73bvasug-a.oregon-postgres.render.com/soko_safi_db"
 )
@@ -27,7 +31,7 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.json.compact = False  # Prettify JSON responses
-    app.secret_key = b'\x9c\x02\x1e\x87\xe8\xde\n\xe0\x91\xe2\x15\xc8\x04\x0e\xde\xb5' # Replace with a real secret key
+    app.secret_key = b'fbc39b2b7e2e42973ec88ef43a265900435fbcc526a9b254a0cd20c1623dd259'
 
     # Initialize extensions
     db.init_app(app)
